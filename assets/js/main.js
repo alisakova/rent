654ba9a966b6c2babf767de856b20646f1904741
alisakova <coctail_rossini@mail.ru>
2018-11-29 23:54:42 +0300

    sections done: contact-us, stage startups

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,36 @@
-'use strict'
+"use strict";
 
 // mobile navigation
-let navLink = document.querySelector('.header__nav-link');
-let navMenu = document.querySelector('.navigation');
+let navLink = document.querySelector(".header__nav-link");
+let navMenu = document.querySelector(".navigation");
 
 function navOpen(el, target) {
-    el.classList.add('active');
-    target.classList.add('visible');
+  el.classList.add("active");
+  target.classList.add("visible");
 }
 
 function navClose(el, target) {
-    el.classList.remove('active');
-    target.classList.remove('visible');
+  el.classList.remove("active");
+  target.classList.remove("visible");
 }
 
-navLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (navLink.classList.contains('active')) {
-        navClose(navLink, navMenu);
-    } else {
-        navOpen(navLink, navMenu);
-    }
+navLink.addEventListener("click", e => {
+  e.preventDefault();
+  if (navLink.classList.contains("active")) {
+    navClose(navLink, navMenu);
+  } else {
+    navOpen(navLink, navMenu);
+  }
 });
 
-//accordion
+$(document).ready(function() {
+  $('.select').select2({
+    minimumResultsForSearch: Infinity
+  });
+});
 
+
+//accordion
 
 // let accordionLink = document.querySelectorAll('[data-accordion="link"]');
 // let i;
@@ -48,33 +54,34 @@ navLink.addEventListener('click', (e) => {
 // }
 
 function getParent(el, elData) {
-    while ((el = el.parentElement) && !el.hasAttribute(elData));
-    return el;
+  while ((el = el.parentElement) && !el.hasAttribute(elData));
+  return el;
 }
 
 function toggleItem(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let elParent = getParent(this, 'data-accordion', 'item');
-    let target = elParent.querySelector('[data-accordion="panel"]');
-    let html = document.querySelector('html');
-    let htmlFontSize = parseFloat(window.getComputedStyle(html, null).getPropertyValue('font-size'));
-    let targetHeight = target.children[0].offsetHeight;
+  let elParent = getParent(this, "data-accordion", "item");
+  let target = elParent.querySelector('[data-accordion="panel"]');
+  let html = document.querySelector("html");
+  let htmlFontSize = parseFloat(
+    window.getComputedStyle(html, null).getPropertyValue("font-size")
+  );
+  let targetHeight = target.children[0].offsetHeight;
 
-    target.style.height = targetHeight/htmlFontSize + 'rem';
-    // if (parent.classList.contains('active')) {
-    //     parent.classList.remove('active');
-    //     target.classList.remove('visible');
-    // } else {
-    //     parent.classList.add('active');
-    //     target.classList.add('visible');
-    // }
+  target.style.height = targetHeight / htmlFontSize + "rem";
+  // if (parent.classList.contains('active')) {
+  //     parent.classList.remove('active');
+  //     target.classList.remove('visible');
+  // } else {
+  //     parent.classList.add('active');
+  //     target.classList.add('visible');
+  // }
 }
 
 let accordionLink = document.querySelectorAll('[data-accordion="link"]');
 let i;
 for (i = 0; i < accordionLink.length; i++) {
-
-    accordionLink[i].addEventListener('click', toggleItem, false);
-    //accordionLink[i].addEventListener('click', toggleItem, false);
+  accordionLink[i].addEventListener("click", toggleItem, false);
+  //accordionLink[i].addEventListener('click', toggleItem, false);
 }
