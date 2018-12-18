82b8760e5b111a73d0146d0342a791caa1f5f7b8
alisakova <coctail_rossini@mail.ru>
2018-12-19 00:11:22 +0300

    fixes

--- build/js/main.js
+++ build/js/main.js
@@ -91,7 +91,7 @@ $(document).ready(function() {
   $("body").on("click", ".subscribe__group a.btn", function(event) {
     event.preventDefault();
     var id = $(this).attr("href"),
-        top = $(id).offset().top;
+        top = $(id).offset().top - 70;
     if ($(".subscribe .form__input").val()) {
       $("body,html").animate({ scrollTop: top }, 1500);
     }
