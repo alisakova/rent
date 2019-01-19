f615fe48e6e8bf4a6d91f47bbbe87b2fbfbacb61
alisakova <coctail_rossini@mail.ru>
2019-01-19 14:17:37 +0300

    fix script

--- build/js/main.js
+++ build/js/main.js
@@ -1,61 +1,65 @@
 let replyBtn = document.querySelectorAll("._reply");
-let replyFormTemplate = document
-  .querySelector(".comments-form-template")
-  .content.querySelector(".comments-form");
+
 const maxLimit = 2000;
 let html = document.querySelector("html");
 let htmlFontSize = parseFloat(
   window.getComputedStyle(html, null).getPropertyValue("font-size")
 );
-let newReplyForm = replyFormTemplate.cloneNode(true);
 
-replyBtn.forEach(function(btn) {
-  btn.addEventListener("click", function(e) {
-    let parentContainer = e.target.closest(".comments-item");
-    let container = parentContainer.querySelector(".comments-item__content");
-    let activeBtn = document.querySelector("._reply.active");
-    if (btn !== activeBtn && activeBtn) {
-      activeBtn.classList.remove("active");
-      let activeContainer = activeBtn.closest(".comments-item");
-      cleanInputs(activeContainer);
-      let form = document.querySelector(".comments-form_answer");
-      form.style.maxHeight = null;
-      setTimeout(function() {
-        container.removeChild(form);
-      }, 250);
-      setTimeout(function() {
+
+if (replyBtn.length > 0) {
+  let replyFormTemplate = document
+    .querySelector(".comments-form-template")
+    .content.querySelector(".comments-form");
+  let newReplyForm = replyFormTemplate.cloneNode(true);
+  replyBtn.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+      let parentContainer = e.target.closest(".comments-item");
+      let container = parentContainer.querySelector(".comments-item__content");
+      let activeBtn = document.querySelector("._reply.active");
+      if (btn !== activeBtn && activeBtn) {
+        activeBtn.classList.remove("active");
+        let activeContainer = activeBtn.closest(".comments-item");
+        cleanInputs(activeContainer);
+        let form = document.querySelector(".comments-form_answer");
+        form.style.maxHeight = null;
+        setTimeout(function() {
+          container.removeChild(form);
+        }, 250);
+        setTimeout(function() {
+          container.appendChild(replyFormTemplate);
+          let targetHeight = replyFormTemplate.children[0].offsetHeight;
+          replyFormTemplate.style.maxHeight = targetHeight / htmlFontSize + "rem";
+          let textarea = container.querySelector(".comments-form textarea");
+          let countSpan = container.querySelector(
+            ".comments-form textarea + .form__counter ._counter"
+          );
+          countSymbols(textarea, countSpan);
+        }, 250);
+      }
+      if (btn.classList.contains("active")) {
+        console.log(1);
+        btn.classList.remove("active");
+        let form = document.querySelector(".comments-form_answer");
+        form.style.maxHeight = null;
+        cleanInputs(container);
+        setTimeout(function() {
+          container.removeChild(form);
+        }, 250);
+      } else {
+        btn.classList.add("active");
         container.appendChild(replyFormTemplate);
-        let targetHeight = replyFormTemplate.children[0].offsetHeight;
-        replyFormTemplate.style.maxHeight = targetHeight / htmlFontSize + "rem";
         let textarea = container.querySelector(".comments-form textarea");
         let countSpan = container.querySelector(
           ".comments-form textarea + .form__counter ._counter"
         );
         countSymbols(textarea, countSpan);
-      }, 250);
-    }
-    if (btn.classList.contains("active")) {
-      console.log(1);
-      btn.classList.remove("active");
-      let form = document.querySelector(".comments-form_answer");
-      form.style.maxHeight = null;
-      cleanInputs(container);
-      setTimeout(function() {
-        container.removeChild(form);
-      }, 250);
-    } else {
-      btn.classList.add("active");
-      container.appendChild(replyFormTemplate);
-      let textarea = container.querySelector(".comments-form textarea");
-      let countSpan = container.querySelector(
-        ".comments-form textarea + .form__counter ._counter"
-      );
-      countSymbols(textarea, countSpan);
-      let targetHeight = replyFormTemplate.children[0].offsetHeight;
-      replyFormTemplate.style.maxHeight = targetHeight / htmlFontSize + "rem";
-    }
+        let targetHeight = replyFormTemplate.children[0].offsetHeight;
+        replyFormTemplate.style.maxHeight = targetHeight / htmlFontSize + "rem";
+      }
+    });
   });
-});
+}
 
 let textarea = document.querySelector(".comments-form textarea");
 let countSpan = document.querySelector(
@@ -89,7 +93,9 @@ var cleanInputs = function(parentContainer) {
   })
 }
 
-countSymbols(textarea, countSpan);
+if (textarea) {
+  countSymbols(textarea, countSpan);
+}
 
 $(document).ready(function() {
   $(".download-form .form__input").change(function() {
