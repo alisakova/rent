cbc14ebe2d456f8a7de1dc3fa2c79dcdcd2d16eb
Lisakova Anastasiia <a.lisakova@osla.ru>
2019-01-22 15:48:51 +0300

    fix footer

--- build/js/main.js
+++ build/js/main.js
@@ -1,53 +1,63 @@
 let replyBtn = document.querySelectorAll("._reply");
-let replyFormTemplate = document.querySelector(".comments-form-template").content.querySelector(".comments-form");
+let replyFormTemplate = document
+  .querySelector(".comments-form-template")
+  .content.querySelector(".comments-form");
 var newReplyForm = replyFormTemplate.cloneNode(true);
 
 replyBtn.forEach(function(btn) {
   btn.addEventListener("click", function(e) {
     let parentContainer = e.target.closest(".comments-item");
     let container = parentContainer.querySelector(".comments-item__content");
-    replyBtn.forEach(function(btn) {
-      if (btn.classList.contains("active")) {
-        btn.classList.remove("active");
-        let form = document.querySelector(".comments-form_answer");
-        form.style.maxHeight = null;
-        setTimeout(function() {
-          container.removeChild(form);
-        }, 1000);
-      }
-    });
+    let replyBtnActive = document.querySelector("._reply.active");
+    let html = document.querySelector("html");
+    let htmlFontSize = parseFloat(
+      window.getComputedStyle(html, null).getPropertyValue("font-size")
+    );
+
     if (btn.classList.contains("active")) {
       btn.classList.remove("active");
       let form = document.querySelector(".comments-form_answer");
       form.style.maxHeight = null;
       setTimeout(function() {
         container.removeChild(form);
-      }, 1000);
+      }, 250);
+    } else if (replyBtnActive && replyBtnActive !== btn) {
+      replyBtnActive.classList.remove("active");
+      let parentOfActiveBtn = replyBtnActive.closest(".comments__item");
+      let containerOfActiveBtn = parentOfActiveBtn.querySelector(".comments-item__content");
+      let form = containerOfActiveBtn.querySelector(".comments-form_answer");
+      form.style.maxHeight = null;
+      setTimeout(function() {
+        container.removeChild(form);
+      }, 250);
+      btn.classList.add("active");
+      container.appendChild(replyFormTemplate);
+      let targetHeight = replyFormTemplate.children[0].offsetHeight;
+      replyFormTemplate.style.maxHeight = targetHeight / htmlFontSize + "rem";
     } else {
       btn.classList.add("active");
       container.appendChild(replyFormTemplate);
       let targetHeight = replyFormTemplate.children[0].offsetHeight;
-      let html = document.querySelector("html");
-      let htmlFontSize = parseFloat(
-        window.getComputedStyle(html, null).getPropertyValue("font-size")
-      );
       replyFormTemplate.style.maxHeight = targetHeight / htmlFontSize + "rem";
     }
-  })
+  });
 });
 
-
-let textarea = document.querySelector(".comments-form textarea");
-let countSpan = document.querySelector(".comments-form textarea + .form__counter ._counter");
+let textarea = document.querySelectorAll(".comments-form textarea");
+let countSpan = document.querySelector(
+  ".comments-form textarea + .form__counter ._counter"
+);
 const maxLimit = 2000;
 
-textarea.addEventListener("input", function() {
-  if (textarea.value.length > maxLimit) {
-    textarea.value = textarea.value.substring(0, maxLimit);
-  } else {
-    countSpan.textContent = maxLimit - textarea.value.length;
-  }
-})
+textarea.forEach(function(area) {
+  area.addEventListener("input", function() {
+    if (area.value.length > maxLimit) {
+      area.value = area.value.substring(0, maxLimit);
+    } else {
+      countSpan.textContent = maxLimit - area.value.length;
+    }
+  });
+});
 
 $(document).ready(function() {
   $(".download-form .form__input").change(function() {
